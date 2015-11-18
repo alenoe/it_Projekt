0b7962c1a29bf30b0f4f6edb7b7f65142777ff75
bobsburgers11 <simon.zahnd@students.fhnw.ch>
2015-11-18 14:14:35 +0100

    18.11.15 Alex GameLogic

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,29 @@
 package application;
 	
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(final Stage stage) throws Exception {
+
+		final URL fxmlUrl = getClass().getResource(
+				"Board.fxml");
+		final FXMLLoader fxmlLoader = new FXMLLoader(fxmlUrl);
+		fxmlLoader.setController(new Dice_Controller());
+		final Parent root = fxmlLoader.load();
+		stage.setScene(new Scene(root, 1000, 700));
+		stage.setTitle("King_of_Tokyo");
+		stage.show();
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(final String[] args) {
 		launch(args);
 	}
 }
